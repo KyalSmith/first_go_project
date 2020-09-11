26693a136a374deb9f9fc1efb4af0060e9fb788c
kyal_es <kyal@elenjicalsolutions.com>
2020-09-11 15:36:51 +0200

    modified hello to make merge conflict

--- src/hello.go
+++ src/hello.go
@@ -4,7 +4,7 @@ import "fmt"
 import "rsc.io/quote"
 
 func Hello() string {
-	fmt.Println(quote.Hello())
+	fmt.Println("Whatever!")
 	fmt.Println(quote.Glass())
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Opt())
