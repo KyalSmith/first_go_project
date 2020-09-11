9b2a89f2b9060aecf190df8ce24d935312c41d1e
kyal_es <kyal@elenjicalsolutions.com>
2020-09-11 16:00:40 +0200

    changed master branch

--- src/hello.go
+++ src/hello.go
@@ -4,7 +4,7 @@ import "fmt"
 import "rsc.io/quote"
 
 func Hello() string {
-    fmt.Println("Hello World!")
+    fmt.Println("Hi There :)")
 	fmt.Println(quote.Glass())
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Opt())
